67ddb423b1147ab68ea6fd09b800295e578f4eb8
Mike Jones, II <micujonesii@gmail.com>
2025-11-28 01:08:46 -0500

    Improve nav experience

--- components/navbar-view/navbar-view.jsx
+++ components/navbar-view/navbar-view.jsx
@@ -1,64 +1,91 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import { NavLink, Link } from 'react-router';
 
 import './navbar-view.css';
+import { useState } from 'react';
 
 export const NavbarView = () => {
+    const [show, setShow] = useState(false);
+
+    const handleShowOffcanvas = () => {
+        show ? setShow(false) : setShow(true);
+    };
+
     return (
         <Navbar
-            expand="lg"
+            expand="md"
             data-bs-theme="dark"
             className="bg-body-tertiary"
             sticky="top"
+            key="md"
         >
-            <Container>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto navbar-menu">
-                        <Nav.Link>
-                            <NavLink
-                                to="/"
-                                className={({ isActive }) =>
-                                    isActive ? 'active' : ''
-                                }
-                            >
-                                Home
-                            </NavLink>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <NavLink
-                                to="/work"
-                                className={({ isActive }) =>
-                                    isActive ? 'active' : ''
-                                }
-                            >
-                                Work
-                            </NavLink>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <NavLink
-                                to="/contact"
-                                className={({ isActive }) =>
-                                    isActive ? 'active' : ''
-                                }
-                            >
-                                Contact
-                            </NavLink>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <NavLink
-                                to="/about"
-                                className={({ isActive }) =>
-                                    isActive ? 'active' : ''
-                                }
-                            >
-                                About
-                            </NavLink>
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+            <Container fluid>
+                <Navbar.Toggle
+                    aria-controls="basic-navbar-nav"
+                    onClick={handleShowOffcanvas}
+                />
+                <Navbar.Offcanvas
+                    placement="end"
+                    data-bs-theme="dark"
+                    aria-labelledby="offcanvasNavbarLabel-expand-md"
+                    show={show}
+                    onHide={handleShowOffcanvas}
+                >
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title>Menu</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="me-auto navbar-menu">
+                            <Nav.Link>
+                                <NavLink
+                                    to="/"
+                                    className={({ isActive }) =>
+                                        isActive ? 'active' : ''
+                                    }
+                                    onClick={handleShowOffcanvas}
+                                >
+                                    Home
+                                </NavLink>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <NavLink
+                                    to="/work"
+                                    className={({ isActive }) =>
+                                        isActive ? 'active' : ''
+                                    }
+                                    onClick={handleShowOffcanvas}
+                                >
+                                    Work
+                                </NavLink>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <NavLink
+                                    to="/contact"
+                                    className={({ isActive }) =>
+                                        isActive ? 'active' : ''
+                                    }
+                                    onClick={handleShowOffcanvas}
+                                >
+                                    Contact
+                                </NavLink>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <NavLink
+                                    to="/about"
+                                    className={({ isActive }) =>
+                                        isActive ? 'active' : ''
+                                    }
+                                    onClick={handleShowOffcanvas}
+                                >
+                                    About
+                                </NavLink>
+                            </Nav.Link>
+                        </Nav>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
             </Container>
         </Navbar>
     );
